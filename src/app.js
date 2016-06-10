9c772ea6085c9016c937d6c7e0729e2049b1b47b
Joachim Viide <jviide@iki.fi>
2016-06-10 12:50:41 +0300

    Add fancier heading

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ class Main extends React.Component {
     render() {
         return (
             <div>
-                <h1 className="heading">Pisteet</h1>
+                <h1 className="heading">'); DROP TABLE scores; --</h1>
                 <Scores scores={this.props.scores} />
             </div>
         );
